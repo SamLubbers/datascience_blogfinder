8cf9e2343773515c9c1418b68898242ef5e5385b
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-13 15:29:11 +0000

    using db connection as context manager and exception handling of duplicate entries

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -2,18 +2,21 @@ import sqlite3
 
 class DatabaseManager(object):
     def __init__(self, db):
-        self.conn = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
-        self.cur = self.conn.cursor()
+        self.con = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
+        self.cur = self.con.cursor()
         self.create_blogs_table()
 
     def create_blogs_table(self):
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS blogs
-                    (url text primary key, source text, title text, pub_date timestamp)''')
-
-        self.conn.commit()
+        with self.con:
+            self.cur.execute('''CREATE TABLE IF NOT EXISTS blogs
+                        (url text primary key, source text, title text, pub_date timestamp)''')
 
     def insert_blog(self, url, source, title, pub_date):
-        self.cur.execute('''INSERT INTO blogs VALUES (?, ?, ?, ?)''',(url, source, title, pub_date))
+        try:
+            with self.con:
+                self.cur.execute('''INSERT INTO blogs VALUES (?, ?, ?, ?)''',(url, source, title, pub_date))
+        except sqlite3.IntegrityError:
+            pass # avoid the same website from being added twice
 
     def __del__(self):
-        self.conn.close()
+        self.con.close()
