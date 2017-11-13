304f7e626df82e6aee1cea4ce170365cfc8350d7
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-13 15:11:19 +0000

    added type support for db, and defined functions to create blogs table and insert entries into that table

--- BlogManager.py
+++ BlogManager.py
@@ -1,11 +1,13 @@
 import threading
 from settings import SENTINEL
+from DatabaseManager import DatabaseManager
 
 
 class BlogManager(threading.Thread):
     def __init__(self, blog_queue):
         super().__init__()
         self.blog_queue = blog_queue
+        self.db_mngr = DatabaseManager('datascience_blogs.db')
 
     def run(self):
         for blog in iter(self.blog_queue.get, SENTINEL):
--- DatabaseManager.py
+++ DatabaseManager.py
@@ -2,8 +2,18 @@ import sqlite3
 
 class DatabaseManager(object):
     def __init__(self, db):
-        self.conn = sqlite3.connect(db)
+        self.conn = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
         self.cur = self.conn.cursor()
+        self.create_blogs_table()
+
+    def create_blogs_table(self):
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS blogs
+                    (url text primary key, source text, title text, pub_date timestamp)''')
+
+        self.conn.commit()
+
+    def insert_blog(self, url, source, title, pub_date):
+        self.cur.execute('''INSERT INTO blogs VALUES (?, ?, ?, ?)''',(url, source, title, pub_date))
 
     def __del__(self):
         self.conn.close()
