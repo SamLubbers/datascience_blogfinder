374d58afc29675af9dcd258e9717669512f94987
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-13 19:42:57 +0000

    autogenerated Blogs model given the schema of our existing database

--- django_frontend/latest_blogs/models.py
+++ django_frontend/latest_blogs/models.py
@@ -1,3 +1,21 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class Blogs(models.Model):
+    url = models.TextField(unique=True, blank=True, null=True)
+    host = models.TextField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    pub_date = models.TextField(blank=True, null=True)  # This field type is a guess.
+
+    class Meta:
+        managed = False
+        db_table = 'blogs'
