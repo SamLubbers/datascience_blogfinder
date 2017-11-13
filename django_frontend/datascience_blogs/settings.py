df743e8e48e8ef6a29a7bca5d84194ad6559bdc2
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-13 19:11:17 +0000

    added latest_blogs app to INSTALLED_APPS list of django project

--- django_frontend/datascience_blogs/settings.py
+++ django_frontend/datascience_blogs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'latest_blogs.apps.LatestBlogsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
