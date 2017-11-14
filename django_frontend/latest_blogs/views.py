d5b869f6696161e66913503827567047e0f68b2f
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-14 10:59:18 +0000

    finished view and template for latest published blogs.

--- django_frontend/latest_blogs/views.py
+++ django_frontend/latest_blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Blogs
 
 def index(request):
-    return render(request, 'latest_blogs/blogs.html')
+    latest_blogs = Blogs.objects.order_by('-pub_date')
+    context = {"latest_blogs": latest_blogs}
+    return render(request, 'latest_blogs/blogs.html', context)
