d9dc935cad778dec127303a38c9b4cdbb0baeb91
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-14 17:29:58 +0000

    application only stores in db those blogs that have been published less than a year ago

--- django_frontend/latest_blogs/views.py
+++ django_frontend/latest_blogs/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Blogs
 
 def index(request):
-    latest_blogs = Blogs.objects.order_by('-pub_date')[:100]
+    latest_blogs = Blogs.objects.order_by('-pub_date')
     context = {"latest_blogs": latest_blogs}
     return render(request, 'latest_blogs/blogs.html', context)
