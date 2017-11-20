a302f6de1b733f12b7ef4bc0b8dd23fa5140251f
Sam~Lubbers <lubbers.sc@gmail.com>
2017-11-20 07:16:39 +0100

    refactored all code onto module level

--- blog_classifier/classifier.py
+++ blog_classifier/classifier.py
@@ -6,26 +6,30 @@ a naive bayes classifier is trained in order to predict if a future extracted bl
 from nltk.corpus import stopwords
 import pandas as pd
 from os import path, getcwd
+
+datasets_dir = path.join(getcwd(), 'datasets')
+dataset_name = 'labelled_blogs.csv'
+dataset_path = path.join(datasets_dir, dataset_name)
+dataset = pd.read_csv(dataset_path)
+
 import re
 import nltk
 nltk.download('stopwords')
 
+corpus = []
 
-def load_dataset(dataset_name):
-    datasets_dir = path.join(getcwd(), 'datasets')
-    dataset_path = path.join(datasets_dir, dataset_name)
-    ds = pd.read_csv(dataset_path)
-    return ds
-
-
-def process_title(title):
+for index, blog in dataset.iterrows():
+    title = blog['title']
     title = re.sub('[^a-zA-Z]', ' ', title)
     title = title.lower()
     title = title.split()
     title = [word for word in title if word not in set(stopwords.words('english'))]
     title = ' '.join(title)
-    return title
+    corpus.append(title)
 
+title_labels = dataset.iloc[:, 1].values
+
+# TODO create count vectorizer
 
 # TODO create train and test datasets
 
@@ -33,15 +37,6 @@ def process_title(title):
 
 # TODO test performance of classifier
 
-if __name__ == '__main__':
-    dataset = load_dataset('labelled_blogs.csv')
-    corpus = []
-    for index, blog in dataset.iterrows():
-        blog_title = blog['title']
-        blog_title = process_title(blog_title)
-        corpus.append(blog_title)
 
-    title_labels = dataset.iloc[:, 1].values
 
-    # TODO get rid of word features, all words etc.
-    # TODO get rid of the below and replace by sklearn feature extractor
+
